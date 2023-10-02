c7bf5b73ffee287fa94a38c746fb510fdc627d78
EmanuelPe01 <emanuel03romo@gmail.com>
2023-10-01 20:48:14 -0600

    Login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Screens/login/login.component';
+import { RegisterComponent } from './Screens/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
